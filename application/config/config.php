709861ffa5894359b91de7f6b50f7112952a3e38
Solanki Vishal <solanki.vishal@supportgov.in>
2024-01-09 18:55:53 +0530

    - Session ISsue Fixed

--- application/config/config.php
+++ application/config/config.php
@@ -382,10 +382,9 @@ $config['encryption_key'] = '';
   | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
   |
  */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 86400;
-$config['sess_driver'] = 'database';
 $config['sess_save_path'] = 'swp_admin_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
